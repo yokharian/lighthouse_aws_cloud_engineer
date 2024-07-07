54ab35f462a1112da30ad476ac5b2a1bc59c9085
Sofia Escobedo <cdmx.sofia@gmail.com>
2024-07-07 15:40:14 -0600

    main stack

--- lighthouse_aws_cloud_engineer/lighthouse_aws_cloud_engineer_stack.py
+++ lighthouse_aws_cloud_engineer/lighthouse_aws_cloud_engineer_stack.py
@@ -1,20 +1,70 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_events as events,
+    aws_events_targets as targets,
+    aws_lambda as _lambda,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_iam as iam
 )
 from constructs import Construct
 
 class LighthouseAwsCloudEngineerStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create a VPC
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LighthouseAwsCloudEngineerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an ECS cluster
+        cluster = ecs.Cluster(self, "Cluster", vpc=vpc)
 
+        # Define a Fargate task definition with a single container
+        task_definition = ecs.FargateTaskDefinition(self, "TaskDef")
+
+        container = task_definition.add_container(
+            "DefaultContainer",
+            image=ecs.ContainerImage.from_registry("public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest"),
+            memory_limit_mib=512,
+            cpu=256,
+        )
+
+        container.add_port_mappings(
+            ecs.PortMapping(container_port=80)
+        )
+
+        # Define a Fargate service
+        ecs_service = ecs.FargateService(
+            self, "Service",
+            cluster=cluster,
+            task_definition=task_definition,
+            desired_count=1,
+            assign_public_ip=True
+        )
+
+        # Define the Lambda function
+        scale_down_function = _lambda.Function(
+            self, "ScaleDownFunction",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="index.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                'CLUSTER_NAME': cluster.cluster_name,
+                'SERVICE_NAME': ecs_service.service_name
+            }
+        )
+
+        # Add permissions for the Lambda to scale down the ECS service
+        scale_down_function.add_to_role_policy(iam.PolicyStatement(
+            actions=["ecs:UpdateService"],
+            resources=[ecs_service.service_arn]
+        ))
+
+        # Define the EventBridge rule
+        rule = events.Rule(
+            self, "Rule",
+            schedule=events.Schedule.cron(minute="0", hour="23")
+        )
+
+        rule.add_target(targets.LambdaFunction(scale_down_function))
