959a22a45abc9852fb1c56dda69f10fdedb8f9f1
Sofia Escobedo <cdmx.sofia@gmail.com>
2024-07-07 16:08:31 -0600

    allowing http traffic

--- lighthouse_aws_cloud_engineer/lighthouse_aws_cloud_engineer_stack.py
+++ lighthouse_aws_cloud_engineer/lighthouse_aws_cloud_engineer_stack.py
@@ -14,8 +14,17 @@ class LighthouseAwsCloudEngineerStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create a VPC
-        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
+        # Create a VPC with only public subnets
+        vpc = ec2.Vpc(
+            self, "MyVpc",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                )
+            ]
+        )
 
         # Create an ECS cluster
         cluster = ecs.Cluster(self, "Cluster", vpc=vpc)
@@ -34,13 +43,29 @@ class LighthouseAwsCloudEngineerStack(Stack):
             ecs.PortMapping(container_port=80)
         )
 
+        # Define security group to allow HTTP traffic
+        security_group = ec2.SecurityGroup(
+            self, "ServiceSG",
+            vpc=vpc,
+            description="Allow http traffic",
+            allow_all_outbound=True
+        )
+
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(80),
+            "Allow HTTP traffic"
+        )
+
         # Define a Fargate service
         ecs_service = ecs.FargateService(
             self, "Service",
             cluster=cluster,
             task_definition=task_definition,
             desired_count=1,
-            assign_public_ip=True
+            assign_public_ip=True,
+            security_groups=[security_group],
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
         )
 
         # Define the Lambda function
